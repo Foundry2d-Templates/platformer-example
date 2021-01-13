c33a893f9e1d214340f9147ba8ac36b64e811903
mundusnine <mundusnine@gmail.com>
2021-01-13 15:13:30 -0500

    Update .gitmodules, Platformer.prj, and 8 more files...

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -6,6 +6,6 @@ class Main {
 
 	public static function main(){
 		//To see the whole FoundConfig, go have a look at Found.hx in foundry2d library
-		Found.setup({app:Project, title:'Foundry2d'});
+		Found.setup({app:Project, title:'Foundry2d',width: 1920,height: 1080});
 	}
 }
